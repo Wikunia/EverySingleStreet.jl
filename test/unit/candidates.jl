51c606f086858d77b75a67e8cd291ef8289e633b
Ole Kröger <o.kroeger@opensourc.es>
2023-01-21 18:12:27 +0100

    compute candidates (#12)
    * compute candidates

---
+++ test/unit/candidates.jl
@@ -0,0 +1,26 @@
+@testset "Candidates" begin
+
+@testset "Single point several candidates" begin
+    city_map = EverySingleStreet.parse_map(joinpath(@__DIR__, "..", "data", "CLZ.json"))
+    p = LLA(51.80665409406621, 10.335359255147063)
+    candidates = EverySingleStreet.get_candidates(city_map, [p])[1]
+    # check whether the following four streets have at least one candidate point
+    test_street_names = ["Windmühlenstraße", "Kronenplatz", "Sorge", "Erzstraße"]
+    for name in test_street_names
+        found = false
+        for candidate in candidates
+            if candidate.way.name == name
+                found = true
+                break
+            end
+        end
+        @test found
+    end
+
+    # check that lla of the candidate is less than 100m away from the initial point p
+    for candidate in candidates
+        @test euclidean_distance(p, candidate.lla) < 100
+    end
+end
+
+end
