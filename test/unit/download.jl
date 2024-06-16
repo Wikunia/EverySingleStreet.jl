9a757dccf231195e535e43ec5d537de6869ee81d
Wikunia <o.kroeger@opensourc.es>
2024-06-16 19:43:22 +0200

    some test cases

--- test/unit/download.jl
+++ test/unit/download.jl
@@ -1,6 +1,6 @@
 @testset "Download CLZ" begin 
-    path = joinpath(@__DIR__, "..", "data", "CLZ.json")
-    EverySingleStreet.download("Clausthal-Zellerfeld, Germany", path)
+    path = joinpath(@__DIR__, "..", "data", "test_download.json")
+    EverySingleStreet.download(LLA(53.0, 9.9), LLA(53.01, 10.01) , path)
     city_map = EverySingleStreet.parse_map(path)
     @test !all(EverySingleStreet.iswalkable, city_map.ways)
     EverySingleStreet.filter_walkable_json!(path)
